64f7b219c7dfd36f89b9daeb3c87561ad6d7b437
Kushal Gowda C R <147250588+blue67chillout@users.noreply.github.com>
2024-06-01 19:51:36 +0530

    Update user_proj_example.v

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -36,7 +36,7 @@
  */
 
 module user_proj_example #(
-    parameter BITS = 16
+    parameter N=7,Div = 2
 )(
 `ifdef USE_POWER_PINS
     inout vccd1,	// User area 1 1.8V supply
@@ -46,111 +46,203 @@ module user_proj_example #(
     // Wishbone Slave ports (WB MI A)
     input wb_clk_i,
     input wb_rst_i,
-    input wbs_stb_i,
-    input wbs_cyc_i,
-    input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
-    output [31:0] wbs_dat_o,
 
-    // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
 
     // IOs
-    input  [BITS-1:0] io_in,
-    output [BITS-1:0] io_out,
-    output [BITS-1:0] io_oeb,
-
-    // IRQ
-    output [2:0] irq
+    input  [17:0] io_in,
+    output [9:0] io_out,
+    output [27:0] io_oeb,
 );
     wire clk;
     wire rst;
-
-    wire [BITS-1:0] rdata; 
-    wire [BITS-1:0] wdata;
-    wire [BITS-1:0] count;
-
-    wire valid;
-    wire [3:0] wstrb;
-    wire [BITS-1:0] la_write;
-
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = {{(32-BITS){1'b0}}, rdata};
-    assign wdata = wbs_dat_i[BITS-1:0];
-
+    wire ack_a;
+    wire en;
+    wire [N-1:0]addr;
+    wire rw;       // write || rw=0 ; read || rw = 1
+    wire [7:0]din;
+    wire scl;
+    wire sda;
+    wire [7:0]dout 
+    
     // IO
-    assign io_out = count;
-    assign io_oeb = {(BITS){rst}};
+    assign io_out[1:0] = {scl,sda};
+    assign io_out[9:2] = dout;
+    assign {din,addr,ack_a,en,rw} = io_in;
+    assign io_oeb = 28'hFFFFC0;
 
-    // IRQ
-    assign irq = 3'b000;	// Unused
 
-    // LA
-    assign la_data_out = {{(128-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:64-BITS] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
+    assign clk =  wb_clk_i;
+    assign rst =  wb_rst_i;
 
-    counter #(
-        .BITS(BITS)
-    ) counter(
+    i2c #(
+        .N(N),.Div(Div)
+    ) i2c(
         .clk(clk),
         .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i[BITS-1:0]),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:64-BITS]),
-        .count(count)
+        .ack_a(ack_a),
+        .en(en),
+        .rw(rw),
+        .addr(addr),
+        .din(din),
+        .dout(dout),
+        .scl(scl),
+        .sda(sda)
     );
 
 endmodule
 
-module counter #(
-    parameter BITS = 16
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output reg ready,
-    output reg [BITS-1:0] rdata,
-    output reg [BITS-1:0] count
+
+module i2c #(
+    parameter N=7,Div = 2
+) (
+   sda,scl,ack_a,clk,rst,addr,rw,en,din,dout
 );
+    output sda; //serial data
+    output scl; //serial clk
+    input ack_a;
+    input clk;
+    input rst;
+    input en;
+    input [N-1:0]addr;
+    input rw;       // write || rw=0 ; read || rw = 1
+    input [7:0]din;
+    
+    output reg [7:0]dout ;// for read mode
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 1'b0;
-            ready <= 1'b0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1'b1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
+localparam IDLE = 4'd1;
+localparam START = 4'd2;
+localparam ADDR = 4'd3;
+localparam ACK0 = 4'd4; //ack for addr verification and rw
+localparam W_DATA = 4'd5;
+localparam ACK1 = 4'd6; //ack for successful write
+localparam R_DATA = 4'd7;
+localparam ACK2 = 4'd8; //ack for successfull read
+localparam STOP = 4'd9;
+
+reg [3:0]state;
+reg [N:0] addrw; // temp for addr and rw desn't affect that much just saves a state
+reg [7:0] saved_data; //temp for din
+reg sda_out;  //temp for sda
+reg clk_temp = 0;
+reg [2:0]count;
+reg [2:0]counter = 0;
+assign scl = (en == 1)?clk_temp:1'b1;
+assign sda = (en == 1)?sda_out:1'b1;
+
+
+//the implementation is a moore machine which changes states a/c to the i2c protocol
+
+
+always@(posedge clk) begin
+    if(counter == Div-1) begin
+        clk_temp <= ~clk_temp;    // clk divider to set relation between
+        counter <= 0;           //   sytem clk and scl 
     end
+    else counter <= counter + 1;
+end
 
+always@(posedge clk_temp or posedge rst ) begin
+    if(rst ) begin
+        state <= IDLE;
+    end
+    else begin
+        case(state)
+            IDLE:  begin
+                state <= START;
+                addrw <= {addr,rw}; 
+            end 
+
+            START: begin
+                state <= ADDR;
+                count <=3'd7;
+                saved_data <= din;
+            end
+
+            ADDR: begin
+                if (count == 0 && addrw[0] == 0) state <= ACK0;
+                else count<= count - 1; 
+            end
+
+            ACK0: begin
+                if(ack_a == 0 && addrw[0] == 0) begin
+                    state <= W_DATA;
+                    count <= 3'd7;
+                end
+                else if(ack_a == 0 && addrw[0] == 1) begin
+                    state <= R_DATA;
+                    count <= 3'd7;
+                end
+                else state <= STOP;
+            end
+
+            W_DATA: begin
+                if(count == 0 )begin
+                    state <= ACK1;
+                end
+                else count <= count - 1;
+            end
+
+            R_DATA: begin
+                if(count == 0 )begin
+                    state <= ACK2;
+                end
+                else count <= count - 1;
+            end
+
+            ACK1: begin
+                if(ack_a == 0) begin
+                    state <= W_DATA;
+                    count <= 3'd7;
+                end
+                else state <= STOP;
+            end
+
+            ACK2: begin
+                if(ack_a == 0) begin
+                    state <= R_DATA;
+                    count <= 3'd7;
+                end
+                else state <= STOP;
+            end
+
+            STOP: begin
+                state <= IDLE;
+            end
+            default:state <= IDLE;
+
+        endcase
+    end
+    
+end
+
+always@(negedge clk_temp or posedge rst ) begin
+    if(rst) begin
+        sda_out <= 1'b1;
+    end
+    else begin
+        case (state)
+            IDLE:sda_out <= 1;
+            ADDR:sda_out <= addrw[count];
+            W_DATA: sda_out <= saved_data[count];
+            R_DATA:dout[count] <= sda_out;
+            ACK2:sda_out <= 0;
+            default:sda_out <= 1; 
+        endcase
+    end
+end
+
+always@(posedge clk_temp or posedge rst ) begin
+    if(rst ) begin
+        sda_out <= 1'b1;
+    end
+    else begin
+        case (state)
+            START: sda_out <=0;   //still have doubt  about the triggering at this point
+            STOP : sda_out <=1;
+
+        endcase
+    end
+end
 endmodule
+
 `default_nettype wire
